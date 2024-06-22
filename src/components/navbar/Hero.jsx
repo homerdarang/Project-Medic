ab9ed09a4d03777b0c4d17f6d23382a92ccac1d9
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-06-22 19:11:04 +0800

    Refectored code in Hero component

--- src/components/navbar/Hero.jsx
+++ src/components/navbar/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import twoDoctors from '../../assets/images/healthWellness.jpg';
+// import twoDoctors from '../../assets/images/healthWellness.jpg';
 
 function Hero() {
     return (
         <>
             <div>
-                <div className='bg-cover bg-center bg-no-repeat lg:h-lvh md:h-svh h-96 hero' style={{ backgroundImage: `url(${twoDoctors})` }}></div>
+                <div className='bg-cover bg-center bg-no-repeat lg:h-lvh md:h-svh h-96 cover--hero' ></div>
             </div>
         </>
     )
