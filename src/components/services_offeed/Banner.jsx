cfda8d848ab39ba81d9537b44f58857f85032432
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-06-27 15:54:09 +0800

    Change of Services Title

--- src/components/services_offeed/Banner.jsx
+++ src/components/services_offeed/Banner.jsx
@@ -11,7 +11,7 @@ function Banner() {
     return (
         <>
             <div className='lg:py-40 md:py-36 py-32 services'>
-                <h1 className='lg:text-5xl md:text-4xl text-3xl mb-8 md:font-bold font-semibold text-blue-400 text-center'>Treatment and Services</h1>
+                <h1 className='lg:text-5xl md:text-4xl text-3xl mb-8 md:font-bold font-semibold text-blue-400 text-center'>Care Solutions</h1>
                 <div className='lg:py-20 lg:px-28 md:px-20 md:py-10 px-7 py-3 font-medium text-blue-400 text-2xl lg:flex lg:justify-center gap-3 grid place-items-center lg:grid-cols-5 lg:gap-x-1 md:gap-x-20 md:grid-cols-3 text-center'>
                     <Link to={'/nutrition-monitoring'} className='bg-white/60 md:w-56 w-80 m-3 lg:px-10 lg:py-5 md:px-8 md:py-4 px-5 py-2 border-[1px] border-dashed border-gray-500 rounded-md hover:shadow-2xl hover:bg-blue-400 hover:text-white hover:border-none  hover:scale-105 transition-all ease-linear grid place-items-center outline-none'>
                         <span className='py-2'><GiFruitBowl /></span>
