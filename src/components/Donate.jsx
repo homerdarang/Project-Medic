9467b68e970261b95ab3b5d9f9c8288dcf0d1e44
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-06-29 00:35:47 +0800

    added slogan to the payment method component

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -15,17 +15,20 @@ function Donate() {
 
   return (
     <>
-        <div className='lg:w-1/2 md:w-2/3 sm:w-3/4 w-4/5 m-auto mt-48'>
-            <div className='border text-gray-500 rounded-3xl px-10 py-20 relative'>
-                <div className='font-semibold md:text-2xl text-xl absolute bg-gray-50 border px-5 py-6 rounded-2xl payment--title text-center'>
-                    <h1>Choose your payment method</h1>
+        <div className='lg:w-1/2 md:w-2/3 sm:w-3/4 w-4/5 m-auto mt-48 '>
+            <div className='border text-gray-700 rounded-3xl px-10 py-20 relative'>
+                <div className='font-semibold absolute bg-gray-50 border px-5 py-6 rounded-2xl payment--title text-center'>
+                    <h1 className='md:text-2xl text-xl'>Choose your payment method</h1>
                 </div>
                 <div className='grid gap-4 md:w-2/3 w-5/6 m-auto'>
                     <button onClick={() => navigate(-1)} className='border-2 ps-5 py-2 max-w-32 hover:bg-gray-50 transition-all ease-linear flex place-items-center'>
                         <RiArrowGoBackFill className='me-2' />
                         Go Back
                     </button>
-                    <p className='text-xl mb-5'>How would you like to donate?</p>
+                    <div className='mb-5 grid'>
+                        <p className='text-xl'>How would you like to donate?</p>
+                        <small className='text-sx text-gray-500'>Together, We Make a Difference ❤️</small>
+                    </div>
                     <div className='border px-4 bg-gray-50 py-2 rounded-md flex justify-between place-items-center'>
                         <p>Gcash</p>
                         <Link to='/gcash' className='outline-none active:scale-95'>
